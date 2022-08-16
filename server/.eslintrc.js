a3408888dcc4197f4b85e363acc9a52651f58455
Avinash <za21abc@herts.ac.uk>
2022-08-16 15:17:47 +0100

    add server side code for our exprress project

---
+++ server/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
